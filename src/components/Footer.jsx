7ce55a770d4cfa94856d8e7049a5d65fea3e97ab
Malsha9886 <96550@fhss.sjp.ac.lk>
2022-02-27 17:25:17 +0530

    Develop Protfolio web application

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container>
+        <h2>Contact Me 
+          <h5>No/32; Imaduwa Road;Galle.</h5>
+          <h5>#94-76 675 89 65 / # 94-77 342 54 23</h5>
+        </h2>
+        {props.children}
+        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+        <a
+          className="badge badge-dark"
+          rel="noopener"
+          href="https://github.com/hashirshoaeb"
+          aria-label="My GitHub"
+        >
+          Malsha Kodagoda
+        </a>{" "}
+        using <i className="fab fa-react" />
+        <p>
+          <small className="text-muted">
+            Project code is open source. Feel free to fork and make your own
+            version.
+          </small>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
